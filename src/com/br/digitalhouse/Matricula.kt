d0472c93bcc96c4ad27c02fc85f718b55369421e
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-25 16:48:17 -0300

    Ajuste matricula

--- src/com/br/digitalhouse/Matricula.kt
+++ src/com/br/digitalhouse/Matricula.kt
@@ -1,5 +1,6 @@
 package com.br.digitalhouse
 import java.util.Date
 
-class Matricula(val aluno: Aluno, val curso: Curso, val data: Date) {
+class Matricula(val aluno: Aluno, val curso: Curso) {
+    val data: Date = Date()
 }
