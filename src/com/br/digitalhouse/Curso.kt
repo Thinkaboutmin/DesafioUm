fc42b78326594ef21d27fc7322088c58f4e86eb1
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-25 16:54:46 -0300

    Implementação do Curso

--- src/com/br/digitalhouse/Curso.kt
+++ src/com/br/digitalhouse/Curso.kt
@@ -1,4 +1,26 @@
 package com.br.digitalhouse
 
-class Curso {
+class Curso(val nome: String, val codigo: Int, qntMaxAlunos: Int) {
+    var professorAdjunto: ProfessorAdjunto? = null
+    var professorPrincipal: ProfessorPrincipal? = null
+    val alunos = mutableListOf<Aluno>()
+
+    override fun equals(other: Any?): Boolean {
+        if (other != null && other is Curso) {
+            return other.codigo == this.codigo
+        }
+
+        return super.equals(other)
+    }
+
+    fun adicionarUmAluno(umAluno: Aluno) {
+        val aluno = alunos.find {it == umAluno}
+        if (aluno == null) {
+            alunos.add(umAluno)
+        }
+    }
+
+    fun excluirAluno(umAluno: Aluno) {
+        alunos.remove(umAluno)
+    }
 }
