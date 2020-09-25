b9e3c1d699d06845c384016d50f2caec94caaa1e
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-25 16:25:25 -0300

    Project Tree

---
+++ src/com/br/digitalhouse/Aluno.kt
@@ -0,0 +1,11 @@
+package com.br.digitalhouse
+
+data class Aluno(val nome: String, val sobrenome: String, val codigo: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (other != null && other is Aluno) {
+            return other.codigo == this.codigo
+        }
+
+        return false
+    }
+}
---
+++ src/com/br/digitalhouse/Professor.kt
@@ -0,0 +1,5 @@
+package com.br.digitalhouse
+
+class Professor(val nome: String, val sobrenome: String, val codigo: Int) {
+
+}
